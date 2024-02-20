0639e84b999c70d09e857c48a52e2ce4b81684cb
ROMemories <romain.fouquet@inria.fr>
2024-02-20 15:43:45 +0100

    WIP: support zero or multiple extractors

--- src/riot-rs-macros/src/main_macro.rs
+++ src/riot-rs-macros/src/main_macro.rs
@@ -14,9 +14,17 @@
 /// this macro is used.
 #[proc_macro_attribute]
 pub fn main(_args: TokenStream, item: TokenStream) -> TokenStream {
+    use std::iter;
+
     use quote::quote;
 
     let task_function = syn::parse_macro_input!(item as syn::ItemFn);
+    let function_param_count = task_function.sig.inputs.len();
+
+    // Add an extractor for each function parameter, so that the function receives the requested
+    // peripherals
+    let extractors =
+        iter::repeat(quote! { peripherals.into_peripherals() }).take(function_param_count);
 
     let riot_rs_crate = utils::riot_rs_crate();
 
@@ -25,8 +33,7 @@ pub fn main(_args: TokenStream, item: TokenStream) -> TokenStream {
         #[linkme(crate = #riot_rs_crate::embassy::linkme)]
         fn __main(spawner: &Spawner, mut peripherals: &mut #riot_rs_crate::embassy::arch::OptionalPeripherals) {
             use #riot_rs_crate::define_peripherals::FromOptionalPeripherals;
-            // FIXME: make this work for 0-n peripheral groups
-            spawner.spawn(main(peripherals.into_peripherals())).unwrap();
+            spawner.spawn(main(#(#extractors),*)).unwrap();
         }
 
         #[embassy_executor::task]
