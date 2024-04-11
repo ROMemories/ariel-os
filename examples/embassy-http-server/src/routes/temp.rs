eb730fd3deb5520fe3bf07780b271aa6a8c6f415
ROMemories <romain.fouquet@inria.fr>
2024-04-11 09:13:02 +0200

    WIP

--- examples/embassy-http-server/src/routes/temp.rs
+++ examples/embassy-http-server/src/routes/temp.rs
@@ -3,9 +3,12 @@ use picoserve::{
     extract::State,
     response::{IntoResponse, Json},
 };
-use riot_rs::{sensors::sensor::{Reading, Sensor}, thread};
+use riot_rs::{
+    sensors::sensor::{Reading, Sensor},
+    thread,
+};
 
-use crate::{TEMP_SENSOR, println};
+use crate::{println, TEMP_SENSOR};
 
 pub async fn temp() -> impl IntoResponse {
     let signal: Signal<CriticalSectionRawMutex, i32> = Signal::new();
@@ -13,25 +16,26 @@ pub async fn temp() -> impl IntoResponse {
     fn read_temp(signal: &Signal<CriticalSectionRawMutex, i32>) {
         // FIXME: handle this unwrap
         let temp = TEMP_SENSOR.read().unwrap().value;
-        println!("Read");
-        // FIXME: HardFaults
+        // println!("read");
         signal.signal(temp);
-        println!("signaled");
+        // println!("signaled");
     }
 
-    let mut stack = [0u8; 1024_usize];
-    println!("will spawn");
+    let mut stack = [0u8; 2048_usize];
+    // println!("will spawn");
     thread::thread_create(read_temp, &signal, &mut stack, 1);
-    println!("thread spawned");
+    // println!("thread spawned");
 
     let temp = signal.wait().await;
+    // println!("waited");
 
     Json(JsonTemp { temp })
 }
 
 #[thread]
-// fn _dummy() {loop {}}
-fn _dummy() {thread::sleep();}
+fn _dummy() {
+    thread::sleep();
+}
 
 #[derive(serde::Serialize)]
 struct JsonTemp {
