9f9a3284b36c67e13670846a19117eaeffff43cd
ROMemories <romain.fouquet@inria.fr>
2024-05-31 15:26:52 +0200

    fixup! feat: provide a crate to obtain interrupt names from peripherals

--- src/riot-rs-interrupts/src/lib.rs
+++ src/riot-rs-interrupts/src/lib.rs
@@ -4,14 +4,14 @@ use std::collections::HashMap;
 use serde::Deserialize;
 
 #[must_use]
-pub fn interrupt_for_peripheral(periperal: &str) -> Vec<ContextInterrupt> {
+pub fn interrupt_for_peripheral(peripheral: &str) -> Vec<ContextInterrupt> {
     let json = include_str!("./interrupts.json");
     let interrupts: Context = serde_json::from_str(json).unwrap();
     interrupts
         .0
         .iter()
         .filter_map(|(context, interrupts)| {
-            if let Some((_, interrupt)) = interrupts.0.iter().find(|(p, _)| *p == periperal) {
+            if let Some((_, interrupt)) = interrupts.0.iter().find(|(p, _)| *p == peripheral) {
                 Some(ContextInterrupt {
                     context: context.to_owned(),
                     interrupt: interrupt.to_owned(),
