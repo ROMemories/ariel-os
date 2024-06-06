2a88900288b696055f683c7dbd2725e8ac9080e2
ROMemories <romain.fouquet@inria.fr>
2024-06-06 09:29:20 +0200

    refactor(hwsetup): factor out nRF SPI drivers definition

--- src/riot-rs-embassy/src/arch/nrf/spi.rs
+++ src/riot-rs-embassy/src/arch/nrf/spi.rs
@@ -62,139 +62,123 @@ impl From<BitOrder> for embassy_nrf::spim::BitOrder {
     }
 }
 
-// FIXME: register only the required interrupts
-bind_interrupts!(
-    struct Irqs {
-        // SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0 => InterruptHandler<peripherals::TWISPI0>;
-        // SPIM1_SPIS1_TWIM1_TWIS1_SPI1_TWI1 => InterruptHandler<peripherals::TWISPI1>;
-        SPIM2_SPIS2_SPI2 => InterruptHandler<peripherals::SPI2>;
-        SPIM3 => InterruptHandler<peripherals::SPI3>;
-    }
-);
-
-macro_rules! define_spi_driver {
-    ($peripheral:ident) => {
+macro_rules! define_spi_drivers {
+    ($( $interrupt:ident => $peripheral:ident ),* $(,)?) => {
+        // paste allows to create new identifiers by concatenation using `[<foo bar>]`.
         paste::paste! {
-            pub struct [<Spi $peripheral>] {
-                spim: Spim<'static, peripherals::$peripheral>,
-            }
-
-            impl [<Spi $peripheral>] {
-                #[must_use]
-                pub fn new(
-                    spim_peripheral: peripherals::$peripheral,
-                    sck_pin: impl GpioPin,
-                    miso_pin: impl GpioPin,
-                    mosi_pin: impl GpioPin,
-                    config: Config,
-                ) -> Self {
-                    let mut spi_config = embassy_nrf::spim::Config::default();
-                    spi_config.frequency = config.frequency;
-                    spi_config.mode = config.mode.into();
-                    spi_config.bit_order = config.bit_order.into();
-
-                    let spim = Spim::new(
-                        spim_peripheral,
-                        Irqs,
-                        sck_pin,
-                        miso_pin,
-                        mosi_pin,
-                        spi_config,
-                    );
-
-                    Self { spim }
+            $(
+                pub struct [<Spi $peripheral>] {
+                    i2c: Spim<'static, peripherals::$peripheral>,
                 }
+
+                impl [<Spi $peripheral>] {
+                    #[must_use]
+                    pub fn new(
+                        spim_peripheral: peripherals::$peripheral,
+                        sck_pin: impl GpioPin,
+                        miso_pin: impl GpioPin,
+                        mosi_pin: impl GpioPin,
+                        config: Config,
+                    ) -> Self {
+                        let mut spi_config = embassy_nrf::spim::Config::default();
+                        spi_config.frequency = config.frequency;
+                        spi_config.mode = config.mode.into();
+                        spi_config.bit_order = config.bit_order.into();
+
+                        bind_interrupts!(
+                            struct Irqs {
+                                $interrupt => InterruptHandler<peripherals::$peripheral>;
+                            }
+                        );
+
+                        let spim = Spim::new(
+                            spim_peripheral,
+                            Irqs,
+                            sck_pin,
+                            miso_pin,
+                            mosi_pin,
+                            spi_config,
+                        );
+
+                        Self { i2c: spim }
+                    }
+                }
+
+                impl embedded_hal_async::spi::ErrorType for [<Spi $peripheral>] {
+                    type Error = embassy_nrf::spim::Error;
+                }
+
+                impl embedded_hal_async::spi::SpiBus for [<Spi $peripheral>] {
+                    async fn read(&mut self, words: &mut [u8]) -> Result<(), Self::Error> {
+                        self.i2c.read(words).await
+                    }
+
+                    async fn write(&mut self, data: &[u8]) -> Result<(), Self::Error> {
+                        self.i2c.write(data).await
+                    }
+
+                    async fn transfer(&mut self, rx: &mut [u8], tx: &[u8]) -> Result<(), Self::Error> {
+                        self.i2c.transfer(rx, tx).await
+                    }
+
+                    async fn transfer_in_place(&mut self, words: &mut [u8]) -> Result<(), Self::Error> {
+                        self.i2c.transfer_in_place(words).await
+                    }
+
+                    async fn flush(&mut self) -> Result<(), Self::Error> {
+                        self.i2c.flush().await
+                    }
+                }
+            )*
+
+            pub enum Spi {
+                $( $peripheral([<Spi $peripheral>]), )*
             }
 
-            impl embedded_hal_async::spi::SpiBus for [<Spi $peripheral>] {
+            impl embedded_hal_async::spi::ErrorType for Spi {
+                type Error = embassy_nrf::spim::Error;
+            }
+
+            impl embedded_hal_async::spi::SpiBus for Spi {
                 async fn read(&mut self, words: &mut [u8]) -> Result<(), Self::Error> {
-                    self.spim.read(words).await
+                    match self {
+                        $( Self::$peripheral(spi) => spi.i2c.read(words).await, )*
+                    }
                 }
 
                 async fn write(&mut self, data: &[u8]) -> Result<(), Self::Error> {
-                    self.spim.write(data).await
+                    match self {
+                        $( Self::$peripheral(spi) => spi.i2c.write(data).await, )*
+                    }
                 }
 
                 async fn transfer(&mut self, rx: &mut [u8], tx: &[u8]) -> Result<(), Self::Error> {
-                    self.spim.transfer(rx, tx).await
+                    match self {
+                        $( Self::$peripheral(spi) => spi.i2c.transfer(rx, tx).await, )*
+                    }
                 }
 
                 async fn transfer_in_place(&mut self, words: &mut [u8]) -> Result<(), Self::Error> {
-                    self.spim.transfer_in_place(words).await
+                    match self {
+                        $( Self::$peripheral(spi) => spi.i2c.transfer_in_place(words).await, )*
+                    }
                 }
 
                 async fn flush(&mut self) -> Result<(), Self::Error> {
-                    self.spim.flush().await
+                    match self {
+                        $( Self::$peripheral(spi) => spi.i2c.flush().await, )*
+                    }
                 }
-            }
-
-            impl embedded_hal_async::spi::ErrorType for [<Spi $peripheral>] {
-                type Error = embassy_nrf::spim::Error;
             }
         }
     };
 }
 
-// define_spi_driver!(TWISPI0);
-// define_spi_driver!(TWISPI1);
-define_spi_driver!(SPI2);
-define_spi_driver!(SPI3);
-
-// TODO: codegen this
-pub enum Spi {
-    // TWISPI0(SpiTWISPI0),
-    // TWISPI1(SpiTWISPI1),
-    SPI2(SpiSPI2),
-    SPI3(SpiSPI3),
-}
-
-impl embedded_hal_async::spi::SpiBus for Spi {
-    async fn read(&mut self, words: &mut [u8]) -> Result<(), Self::Error> {
-        match self {
-            // Self::TWISPI0(spi) => spi.spim.read(words).await,
-            // Self::TWISPI1(spi) => spi.spim.read(words).await,
-            Self::SPI2(spi) => spi.spim.read(words).await,
-            Self::SPI3(spi) => spi.spim.read(words).await,
-        }
-    }
-
-    async fn write(&mut self, data: &[u8]) -> Result<(), Self::Error> {
-        match self {
-            // Self::TWISPI0(spi) => spi.spim.write(data).await,
-            // Self::TWISPI1(spi) => spi.spim.write(data).await,
-            Self::SPI2(spi) => spi.spim.write(data).await,
-            Self::SPI3(spi) => spi.spim.write(data).await,
-        }
-    }
-
-    async fn transfer(&mut self, rx: &mut [u8], tx: &[u8]) -> Result<(), Self::Error> {
-        match self {
-            // Self::TWISPI0(spi) => spi.spim.transfer(rx, tx).await,
-            // Self::TWISPI1(spi) => spi.spim.transfer(rx, tx).await,
-            Self::SPI2(spi) => spi.spim.transfer(rx, tx).await,
-            Self::SPI3(spi) => spi.spim.transfer(rx, tx).await,
-        }
-    }
-
-    async fn transfer_in_place(&mut self, words: &mut [u8]) -> Result<(), Self::Error> {
-        match self {
-            // Self::TWISPI0(spi) => spi.spim.transfer_in_place(words).await,
-            // Self::TWISPI1(spi) => spi.spim.transfer_in_place(words).await,
-            Self::SPI2(spi) => spi.spim.transfer_in_place(words).await,
-            Self::SPI3(spi) => spi.spim.transfer_in_place(words).await,
-        }
-    }
-
-    async fn flush(&mut self) -> Result<(), Self::Error> {
-        match self {
-            // Self::TWISPI0(spi) => spi.spim.flush().await,
-            // Self::TWISPI1(spi) => spi.spim.flush().await,
-            Self::SPI2(spi) => spi.spim.flush().await,
-            Self::SPI3(spi) => spi.spim.flush().await,
-        }
-    }
-}
-
-impl embedded_hal_async::spi::ErrorType for Spi {
-    type Error = embassy_nrf::spim::Error;
-}
+// Define a driver per peripheral
+define_spi_drivers!(
+    // FIXME
+    // SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0 => TWISPI0,
+    // SPIM1_SPIS1_TWIM1_TWIS1_SPI1_TWI1 => TWISPI1,
+    SPIM2_SPIS2_SPI2 => SPI2,
+    SPIM3 => SPI3,
+);
