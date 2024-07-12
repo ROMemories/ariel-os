7186cffd259d6dae640657d92b08d793c6d38994
ROMemories <romain.fouquet@inria.fr>
2024-07-12 14:28:03 +0200

    refactor(blinky-example): move the pin definitions to their own module

--- examples/blinky/src/main.rs
+++ examples/blinky/src/main.rs
@@ -3,78 +3,20 @@
 #![feature(type_alias_impl_trait)]
 #![feature(used_with_arg)]
 
+mod pins;
+
 use embassy_time::{Duration, Timer};
-use riot_rs::embassy::{
-    arch::peripherals,
-    gpio::{DriveStrength, Input, Level, Output, Pull},
-};
+use riot_rs::embassy::gpio::{Input, Level, Output, Pull};
 
-#[cfg(context = "nrf52840dk")]
-riot_rs::define_peripherals!(BlinkyPeripherals { led1: P0_13 });
-
-#[cfg(context = "nrf52840dk")]
-riot_rs::define_peripherals!(BlinkyButtonPeripherals {
-    led2: P0_14,
-    btn2: P0_12,
-});
-
-#[cfg(context = "nrf5340dk")]
-riot_rs::define_peripherals!(BlinkyPeripherals { led1: P0_28 });
-
-#[cfg(context = "nrf5340dk")]
-riot_rs::define_peripherals!(BlinkyButtonPeripherals {
-    led2: P0_29,
-    btn2: P0_24,
-});
-
-#[cfg(context = "rp")]
-riot_rs::define_peripherals!(BlinkyPeripherals { led1: PIN_1 });
-
-#[cfg(context = "rp")]
-riot_rs::define_peripherals!(BlinkyButtonPeripherals {
-    led2: PIN_2,
-    btn2: PIN_6,
-});
-
-#[cfg(context = "esp")]
-riot_rs::define_peripherals!(BlinkyPeripherals { led1: GPIO_0 });
-
-#[cfg(context = "esp")]
-riot_rs::define_peripherals!(BlinkyButtonPeripherals {
-    led2: GPIO_1,
-    btn2: GPIO_2,
-});
-
-#[cfg(context = "stm32")]
-riot_rs::define_peripherals!(BlinkyPeripherals { led1: PA6 });
-
-#[cfg(context = "stm32")]
-riot_rs::define_peripherals!(BlinkyButtonPeripherals {
-    led2: PA7,
-    btn2: PA9,
-});
+use pins::{BlinkyButtonPeripherals, BlinkyPeripherals};
 
 #[riot_rs::task(autostart, peripherals)]
 async fn blinky(peripherals: BlinkyPeripherals) {
-    // All of the following are possible (not all of them are equivalent):
-    //
-    // let mut led1 = Output::new(peripherals.led1, Level::High);
-    //
-    let mut led1 = Output::builder(peripherals.led1, Level::Low)
-        .opt_drive_strength(DriveStrength::default())
-        .build();
-    //
-    // #[cfg(context = "nrf")]
-    // let mut led1 = Output::builder(peripherals.led1, Level::High)
-    //     .drive_strength(DriveStrength::Medium)
-    //     .build();
-    //
-    // #[cfg(context = "nrf")]
-    // let mut led1 = Output::builder(peripherals.led1, Level::High)
-    //     .drive_strength(DriveStrength::Arch(
-    //         riot_rs::embassy::arch::DriveStrength::High,
-    //     ))
-    //     .build();
+    let mut led1 = Output::new(peripherals.led1, Level::Low);
+
+    // The micro:bit uses an LED matrix; pull the column line low.
+    #[cfg(context = "microbit-v2")]
+    let _led_col1 = Output::new(peripherals.led_col1, Level::Low);
 
     loop {
         led1.toggle();
@@ -84,10 +26,9 @@ async fn blinky(peripherals: BlinkyPeripherals) {
 
 #[riot_rs::task(autostart, peripherals)]
 async fn blinky_button(peripherals: BlinkyButtonPeripherals) {
-    let btn2_builder = Input::builder(peripherals.btn2, Pull::Up);
-    #[cfg(context = "rp")]
-    let btn2_builder = btn2_builder.schmitt_trigger(true);
-    let mut btn2 = btn2_builder.build_with_interrupt().unwrap();
+    let mut btn2 = Input::builder(peripherals.btn2, Pull::Up)
+        .build_with_interrupt()
+        .unwrap();
 
     let mut led2 = Output::new(peripherals.led2, Level::High);
 
