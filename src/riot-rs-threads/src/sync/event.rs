bd51d642e5d13825c0d1a8c9237e140670e9f490
Kaspar Schleiser <kaspar@schleiser.de>
2024-11-07 10:46:26 +0100

    feat(threads): introduce `sync::Event`

---
+++ src/riot-rs-threads/src/sync/event.rs
@@ -0,0 +1,112 @@
+//! This module provides an event that can be waited for.
+//!
+use core::cell::UnsafeCell;
+
+use crate::{threadlist::ThreadList, ThreadState};
+
+/// A Event object.
+///
+/// An [`Event`] can be used to notify multiple threads that some event has happened.
+///
+/// An Event object manages an internal flag that can be set to true with the set() method and reset
+/// to false with the clear() method. The wait() method blocks until the flag is set to true. The
+/// flag is set to false initially.
+pub struct Event {
+    state: UnsafeCell<LockState>,
+}
+
+unsafe impl Sync for Event {}
+
+enum LockState {
+    Unlocked,
+    Locked(ThreadList),
+}
+
+impl Event {
+    /// Creates new **unset** Event.
+    pub const fn new() -> Self {
+        Self {
+            state: UnsafeCell::new(LockState::Locked(ThreadList::new())),
+        }
+    }
+
+    /// Creates new **set** Event.
+    pub const fn new_set() -> Self {
+        Self {
+            state: UnsafeCell::new(LockState::Unlocked),
+        }
+    }
+
+    /// Returns the current state.
+    ///
+    /// true if locked, false otherwise
+    pub fn is_set(&self) -> bool {
+        critical_section::with(|_| {
+            let state = unsafe { &*self.state.get() };
+            matches!(state, LockState::Unlocked)
+        })
+    }
+
+    /// Wait for this [`Event`] to be set (blocking).
+    ///
+    /// If the event was set, this function returns directly.
+    /// If the event was unset, this function will block the current thread until
+    /// the event gets set elsewhere.
+    ///
+    /// # Panics
+    ///
+    /// Panics if this is called outside of a thread context.
+    pub fn wait(&self) {
+        critical_section::with(|cs| {
+            let state = unsafe { &mut *self.state.get() };
+            match state {
+                LockState::Unlocked => (),
+                LockState::Locked(waiters) => {
+                    waiters.put_current(cs, ThreadState::LockBlocked);
+                }
+            }
+        })
+    }
+
+    /// Clear the event (non-blocking).
+    ///
+    /// If the event was set, it will be cleared and the function returns true.
+    /// If the event was unset, the function returns false
+    pub fn clear(&self) -> bool {
+        critical_section::with(|_| {
+            let state = unsafe { &mut *self.state.get() };
+            match state {
+                LockState::Unlocked => {
+                    *state = LockState::Locked(ThreadList::new());
+                    true
+                }
+                LockState::Locked(_) => false,
+            }
+        })
+    }
+
+    /// Set the event.
+    ///
+    /// If the event was unset, and there were waiters, all waiters will be
+    /// woken up.
+    /// If the event was already set, the function just returns.
+    pub fn set(&self) {
+        critical_section::with(|cs| {
+            let state = unsafe { &mut *self.state.get() };
+            match state {
+                LockState::Unlocked => {}
+                LockState::Locked(waiters) => {
+                    // unlock all waiters
+                    while waiters.pop(cs).is_some() {}
+                    *state = LockState::Unlocked;
+                }
+            }
+        })
+    }
+}
+
+impl Default for Event {
+    fn default() -> Self {
+        Self::new()
+    }
+}
