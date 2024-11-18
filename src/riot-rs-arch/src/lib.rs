575d29a811b23019afe6daf71ed170a1665032e2
ROMemories <152802150+ROMemories@users.noreply.github.com>
2024-11-18 15:59:00 +0100

    fixup! docs(arch): document where items from the `arch` mod are coming from

--- src/riot-rs-arch/src/lib.rs
+++ src/riot-rs-arch/src/lib.rs
@@ -3,12 +3,18 @@
 //! This module dispatches between one of the following crate, depending on the target
 //! architecture:
 //!
-//! | Manufacturer         | MCU family  | Items imported                                       |
-//! | -------------------- | ----------- | ---------------------------------------------------- |
-//! | Espressif            | ESP32       | [`riot-rs-esp::*`](../../riot_rs_esp/index.html)     |
-//! | Nordic Semiconductor | nRF         | [`riot-rs-nrf::*`](../../riot_rs_nrf/index.html)     |
-//! | Raspberry Pi         | RP          | [`riot-rs-rp::*`](../../riot_rs_rp/index.html)       |
-//! | STMicroelectronics   | STM32       | [`riot-rs-stm32::*`](../../riot_rs_stm32/index.html) |
+//! | Manufacturer         | MCU family  | Docs rendered for | Items imported                                       |
+//! | -------------------- | ----------- | ----------------- | ---------------------------------------------------- |
+//! | Espressif            | ESP32       | ESP32-C6          | [`riot-rs-esp::*`](../../riot_rs_esp/index.html)     |
+//! | Nordic Semiconductor | nRF         | nRF52840          | [`riot-rs-nrf::*`](../../riot_rs_nrf/index.html)     |
+//! | Raspberry Pi         | RP          | RP2040            | [`riot-rs-rp::*`](../../riot_rs_rp/index.html)       |
+//! | STMicroelectronics   | STM32       | STM32W55RGVX      | [`riot-rs-stm32::*`](../../riot_rs_stm32/index.html) |
+//!
+//! Documentation is only rendered for the MCUs listed in the table above, but [many others are
+//! supported](https://future-proof-iot.github.io/RIOT-rs/dev/docs/book/hardware_functionality_support.html).
+//! To render the docs locally for the MCU of your choice, adapt [the `cargo doc` command used to
+//! generate documentation for the relevant
+//! crate](https://github.com/future-proof-iot/RIOT-rs/blob/main/.github/workflows/build-deploy-docs.yml).
 
 #![no_std]
 
