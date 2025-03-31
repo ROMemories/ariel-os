75b72c1ba4605080a3e6d786c2cf00f8d7f2937f
ROMemories <152802150+ROMemories@users.noreply.github.com>
2025-03-31 17:11:22 +0200

    feat(stm32): allow to provide RCC config from OoT applications

--- src/ariel-os-stm32/src/lib.rs
+++ src/ariel-os-stm32/src/lib.rs
@@ -68,7 +68,7 @@ pub static EXECUTOR: Executor = Executor::new();
 #[must_use]
 pub fn init() -> OptionalPeripherals {
     let mut config = Config::default();
-    board_config(&mut config);
+    config.rcc = clock_config();
 
     #[cfg(not(capability = "hw/stm32-dual-core"))]
     let peripherals = embassy_stm32::init(config);
@@ -79,105 +79,123 @@ pub fn init() -> OptionalPeripherals {
     OptionalPeripherals::from(peripherals)
 }
 
-// TODO: find better place for this
-fn board_config(config: &mut Config) {
-    #[cfg(context = "st-nucleo-wb55")]
+fn clock_config() -> embassy_stm32::rcc::Config {
+    #[cfg(not(feature = "rcc-config-override"))]
     {
-        use embassy_stm32::rcc::*;
+        #[cfg(context = "st-nucleo-wb55")]
+        {
+            use embassy_stm32::rcc::*;
 
-        config.rcc.hsi48 = Some(Hsi48Config {
-            sync_from_usb: true,
-        }); // needed for USB
-        config.rcc.sys = Sysclk::PLL1_R;
-        config.rcc.hse = Some(Hse {
-            freq: embassy_stm32::time::Hertz(32000000),
-            mode: HseMode::Oscillator,
-            prescaler: HsePrescaler::DIV1,
-        });
-        config.rcc.pll = Some(Pll {
-            source: PllSource::HSE,
-            prediv: PllPreDiv::DIV2,
-            mul: PllMul::MUL10,
-            divp: None,
-            divq: None,
-            divr: Some(PllRDiv::DIV2), // sysclk 80Mhz (32 / 2 * 10 / 2)
-        });
-        config.rcc.mux.clk48sel = mux::Clk48sel::HSI48;
+            let mut rcc = embassy_stm32::rcc::Config::default();
+
+            rcc.hsi48 = Some(Hsi48Config {
+                sync_from_usb: true,
+            }); // needed for USB
+            rcc.sys = Sysclk::PLL1_R;
+            rcc.hse = Some(Hse {
+                freq: embassy_stm32::time::Hertz(32000000),
+                mode: HseMode::Oscillator,
+                prescaler: HsePrescaler::DIV1,
+            });
+            rcc.pll = Some(Pll {
+                source: PllSource::HSE,
+                prediv: PllPreDiv::DIV2,
+                mul: PllMul::MUL10,
+                divp: None,
+                divq: None,
+                divr: Some(PllRDiv::DIV2), // sysclk 80Mhz (32 / 2 * 10 / 2)
+            });
+            rcc.mux.clk48sel = mux::Clk48sel::HSI48;
+
+            rcc
+        }
+
+        #[cfg(context = "st-nucleo-f767zi")]
+        {
+            use embassy_stm32::rcc::*;
+            config.rcc.hse = Some(Hse {
+                freq: embassy_stm32::time::Hertz(8000000),
+                mode: HseMode::Bypass,
+            });
+            config.rcc.pll_src = PllSource::HSE;
+            config.rcc.pll = Some(Pll {
+                prediv: PllPreDiv::DIV4,
+                mul: PllMul::MUL216,
+                divp: Some(PllPDiv::DIV2),
+                divq: None,
+                divr: None,
+            });
+            config.rcc.ahb_pre = AHBPrescaler::DIV1;
+            config.rcc.apb1_pre = APBPrescaler::DIV4;
+            config.rcc.apb2_pre = APBPrescaler::DIV2;
+            config.rcc.sys = Sysclk::PLL1_P;
+        }
+
+        #[cfg(context = "stm32h755zi")]
+        {
+            use embassy_stm32::rcc::*;
+
+            let mut rcc = embassy_stm32::rcc::Config::default();
+
+            rcc.hsi = Some(HSIPrescaler::DIV1);
+            rcc.csi = true;
+            rcc.hsi48 = Some(Hsi48Config {
+                sync_from_usb: true,
+            }); // needed for USB
+            rcc.pll1 = Some(Pll {
+                source: PllSource::HSI,
+                prediv: PllPreDiv::DIV4,
+                mul: PllMul::MUL50,
+                divp: Some(PllDiv::DIV2),
+                // Required for SPI (configured by `spi123sel`)
+                divq: Some(PllDiv::DIV16), // FIXME: adjust this divider
+                divr: None,
+            });
+            rcc.sys = Sysclk::PLL1_P; // 400 Mhz
+            rcc.ahb_pre = AHBPrescaler::DIV2; // 200 Mhz
+            rcc.apb1_pre = APBPrescaler::DIV2; // 100 Mhz
+            rcc.apb2_pre = APBPrescaler::DIV2; // 100 Mhz
+            rcc.apb3_pre = APBPrescaler::DIV2; // 100 Mhz
+            rcc.apb4_pre = APBPrescaler::DIV2; // 100 Mhz
+            rcc.voltage_scale = VoltageScale::Scale1;
+            // Set SMPS power config otherwise MCU will not powered after next power-off
+            rcc.supply_config = SupplyConfig::DirectSMPS;
+            rcc.mux.usbsel = mux::Usbsel::HSI48;
+            // Select the clock signal used for SPI1, SPI2, and SPI3.
+            // FIXME: what to do about SPI4, SPI5, and SPI6?
+            rcc.mux.spi123sel = mux::Saisel::PLL1_Q; // Reset value
+
+            rcc
+        }
+
+        #[cfg(context = "stm32u083mc")]
+        {
+            use embassy_stm32::rcc::*;
+
+            config.rcc.hsi48 = Some(Hsi48Config {
+                sync_from_usb: true,
+            }); // needed for USB
+            // No HSE fitted on the stm32u083c-dk board
+            config.rcc.hsi = true;
+            config.rcc.sys = Sysclk::PLL1_R;
+            config.rcc.pll = Some(Pll {
+                source: PllSource::HSI,
+                prediv: PllPreDiv::DIV1,
+                mul: PllMul::MUL7,
+                divp: None,
+                divq: None,
+                divr: Some(PllRDiv::DIV2), // sysclk 56Mhz
+            });
+            config.rcc.mux.clk48sel = mux::Clk48sel::HSI48;
+        }
     }
 
-    #[cfg(context = "st-nucleo-f767zi")]
+    #[cfg(feature = "rcc-config-override")]
     {
-        use embassy_stm32::rcc::*;
-        config.rcc.hse = Some(Hse {
-            freq: embassy_stm32::time::Hertz(8000000),
-            mode: HseMode::Bypass,
-        });
-        config.rcc.pll_src = PllSource::HSE;
-        config.rcc.pll = Some(Pll {
-            prediv: PllPreDiv::DIV4,
-            mul: PllMul::MUL216,
-            divp: Some(PllPDiv::DIV2),
-            divq: None,
-            divr: None,
-        });
-        config.rcc.ahb_pre = AHBPrescaler::DIV1;
-        config.rcc.apb1_pre = APBPrescaler::DIV4;
-        config.rcc.apb2_pre = APBPrescaler::DIV2;
-        config.rcc.sys = Sysclk::PLL1_P;
-    }
-
-    #[cfg(context = "stm32h755zi")]
-    {
-        use embassy_stm32::rcc::*;
-
-        config.rcc.hsi = Some(HSIPrescaler::DIV1);
-        config.rcc.csi = true;
-        config.rcc.hsi48 = Some(Hsi48Config {
-            sync_from_usb: true,
-        }); // needed for USB
-        config.rcc.pll1 = Some(Pll {
-            source: PllSource::HSI,
-            prediv: PllPreDiv::DIV4,
-            mul: PllMul::MUL50,
-            divp: Some(PllDiv::DIV2),
-            // Required for SPI (configured by `spi123sel`)
-            divq: Some(PllDiv::DIV16), // FIXME: adjust this divider
-            divr: None,
-        });
-        config.rcc.sys = Sysclk::PLL1_P; // 400 Mhz
-        config.rcc.ahb_pre = AHBPrescaler::DIV2; // 200 Mhz
-        config.rcc.apb1_pre = APBPrescaler::DIV2; // 100 Mhz
-        config.rcc.apb2_pre = APBPrescaler::DIV2; // 100 Mhz
-        config.rcc.apb3_pre = APBPrescaler::DIV2; // 100 Mhz
-        config.rcc.apb4_pre = APBPrescaler::DIV2; // 100 Mhz
-        config.rcc.voltage_scale = VoltageScale::Scale1;
-        // Set SMPS power config otherwise MCU will not powered after next power-off
-        config.rcc.supply_config = SupplyConfig::DirectSMPS;
-        config.rcc.mux.usbsel = mux::Usbsel::HSI48;
-        // Select the clock signal used for SPI1, SPI2, and SPI3.
-        // FIXME: what to do about SPI4, SPI5, and SPI6?
-        config.rcc.mux.spi123sel = mux::Saisel::PLL1_Q; // Reset value
-    }
-
-    #[cfg(context = "stm32u083mc")]
-    {
-        use embassy_stm32::rcc::*;
-
-        config.rcc.hsi48 = Some(Hsi48Config {
-            sync_from_usb: true,
-        }); // needed for USB
-        // No HSE fitted on the stm32u083c-dk board
-        config.rcc.hsi = true;
-        config.rcc.sys = Sysclk::PLL1_R;
-        config.rcc.pll = Some(Pll {
-            source: PllSource::HSI,
-            prediv: PllPreDiv::DIV1,
-            mul: PllMul::MUL7,
-            divp: None,
-            divq: None,
-            divr: Some(PllRDiv::DIV2), // sysclk 56Mhz
-        });
-        config.rcc.mux.clk48sel = mux::Clk48sel::HSI48;
+        unsafe extern "Rust" {
+            fn __ariel_os_rcc_config() -> embassy_stm32::rcc::Config;
+        }
+        unsafe { __ariel_os_rcc_config() }
     }
 
     // mark used
