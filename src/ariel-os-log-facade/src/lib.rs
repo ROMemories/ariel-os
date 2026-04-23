28cb25232af97d5e720d210777b5e7aed4c1b4cf
ROMemories <152802150+ROMemories@users.noreply.github.com>
2026-04-23 12:17:26 +0200

    refactor(logging): move the logging macros into a dedicated crate
    Signed-off-by: ROMemories <152802150+ROMemories@users.noreply.github.com>

---
+++ src/ariel-os-log-facade/src/lib.rs
@@ -0,0 +1,291 @@
+//! Provides logging macros.
+//!
+//! # Syntax of formatting strings
+//!
+//! The behavior of the provided logging macros depends on which Cargo feature is enabled:
+//! - When the `defmt` feature is enabled, `defmt` is used for logging.
+//! - When the `log` feature is enabled, `log` is used for logging.
+//! - Otherwise, the logging macros are no-ops.
+//!
+//! This means that the syntax of the formatting strings differs depending on the enabled Cargo
+//! feature; please refer to the documentation of those crates for details on the supported syntax.
+
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(nightly, feature(doc_cfg))]
+#![deny(missing_docs)]
+
+#[featurecomb::comb]
+mod _featurecomb {}
+
+// This module is hidden in the docs, but would still be imported by a wildcard import of this
+// crate's items.
+#[doc(hidden)]
+pub mod hidden_for_logging_macros {
+    // Required so the macros can access it.
+    #[cfg(feature = "defmt")]
+    pub use defmt;
+}
+
+#[cfg(feature = "defmt")]
+pub use defmt::{Debug2Format, Display2Format};
+
+#[cfg(feature = "log")]
+#[doc(hidden)]
+pub mod log {
+    use core::fmt::{Debug, Display, Formatter};
+
+    // Re-export only the minimum set of items to minimize breaking changes in case `log`
+    // adds/removes any items.
+    pub use log::{debug, error, info, trace, warn};
+
+    /// No-op wrapper that formats the Debug trait (drop-in replacement for the equivalent `defmt`
+    /// type).
+    pub struct Debug2Format<T: Debug>(pub T);
+
+    impl<T: Debug> Debug for Debug2Format<T> {
+        fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), core::fmt::Error> {
+            self.0.fmt(f)
+        }
+    }
+
+    /// No-op wrapper that formats the Display trait (drop-in replacement for the equivalent
+    /// `defmt` type).
+    pub struct Display2Format<T: Display>(pub T);
+
+    impl<T: Display> Display for Display2Format<T> {
+        fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), core::fmt::Error> {
+            self.0.fmt(f)
+        }
+    }
+}
+
+#[cfg(feature = "log")]
+pub use log::{Debug2Format, Display2Format};
+
+// NOTE: log macros are defined within private modules so that `doc_cfg` does not produce
+// "feature flairs" on them.
+// The macros are still exported even though they are defined "within" private modules.
+//
+// The `if true` conditionals ensure that the arguments are also valid under format_args!; this is
+// requires because a laze switch can just make the back-end switch over. The actual and the unused
+// formatting need to be in different branches to avoid trouble due to arguments being moved.
+#[cfg(feature = "defmt")]
+mod log_macros {
+    /// Logs a message at the trace level.
+    #[macro_export]
+    macro_rules! trace {
+        ($($arg:tt)*) => {{
+            use $crate::hidden_for_logging_macros::defmt;
+            if true {
+                defmt::trace!($($arg)*);
+            } else {
+                let _ = format_args!($($arg)*);
+            }
+        }};
+    }
+
+    /// Logs a message at the debug level.
+    #[macro_export]
+    macro_rules! debug {
+        ($($arg:tt)*) => {{
+            use $crate::hidden_for_logging_macros::defmt;
+            if true {
+                defmt::debug!($($arg)*);
+            } else {
+                let _ = format_args!($($arg)*);
+            }
+        }};
+    }
+
+    /// Logs a message at the info level.
+    #[macro_export]
+    macro_rules! info {
+        ($($arg:tt)*) => {{
+            use $crate::hidden_for_logging_macros::defmt;
+            if true {
+                defmt::info!($($arg)*);
+            } else {
+                let _ = format_args!($($arg)*);
+            }
+        }};
+    }
+
+    /// Logs a message at the warn level.
+    #[macro_export]
+    macro_rules! warn {
+        ($($arg:tt)*) => {{
+            use $crate::hidden_for_logging_macros::defmt;
+            if true {
+                defmt::warn!($($arg)*);
+            } else {
+                let _ = format_args!($($arg)*);
+            }
+        }};
+    }
+
+    /// Logs a message at the error level.
+    #[macro_export]
+    macro_rules! error {
+        ($($arg:tt)*) => {{
+            use $crate::hidden_for_logging_macros::defmt;
+            if true {
+                defmt::error!($($arg)*);
+            } else {
+                let _ = format_args!($($arg)*);
+            }
+        }};
+    }
+
+    /// Prints to the logging output, with a newline.
+    #[macro_export]
+    macro_rules! println {
+        ($($arg:tt)*) => {{
+            use $crate::hidden_for_logging_macros::defmt;
+            if true {
+                defmt::println!($($arg)*);
+            } else {
+                let _ = format_args!($($arg)*);
+            }
+        }};
+    }
+}
+
+#[cfg(feature = "log")]
+mod log_macros {
+    /// Logs a message at the trace level.
+    #[macro_export]
+    macro_rules! trace {
+        ($($arg:tt)*) => {{
+            $crate::log::trace!($($arg)*);
+        }};
+    }
+
+    /// Logs a message at the debug level.
+    #[macro_export]
+    macro_rules! debug {
+        ($($arg:tt)*) => {{
+            $crate::log::debug!($($arg)*);
+        }};
+    }
+
+    /// Logs a message at the info level.
+    #[macro_export]
+    macro_rules! info {
+        ($($arg:tt)*) => {{
+            $crate::log::info!($($arg)*);
+        }};
+    }
+
+    /// Logs a message at the warn level.
+    #[macro_export]
+    macro_rules! warn {
+        ($($arg:tt)*) => {{
+            $crate::log::warn!($($arg)*);
+        }};
+    }
+
+    /// Logs a message at the error level.
+    #[macro_export]
+    macro_rules! error {
+        ($($arg:tt)*) => {{
+            $crate::log::error!($($arg)*);
+        }};
+    }
+
+    /// Prints to the logging output, with a newline.
+    #[macro_export]
+    macro_rules! println {
+        ($($arg:tt)*) => {{
+            $crate::log::println!($($arg)*);
+        }};
+    }
+}
+
+// Define no-op macros in case no facade is enabled.
+#[cfg(not(any(feature = "defmt", feature = "log")))]
+mod log_macros {
+    /// Logs a message at the trace level.
+    #[macro_export]
+    macro_rules! trace {
+        ($($arg:tt)*) => {};
+    }
+
+    /// Logs a message at the debug level.
+    #[macro_export]
+    macro_rules! debug {
+        ($($arg:tt)*) => {};
+    }
+
+    /// Logs a message at the info level.
+    #[macro_export]
+    macro_rules! info {
+        ($($arg:tt)*) => {};
+    }
+
+    /// Logs a message at the warn level.
+    #[macro_export]
+    macro_rules! warn {
+        ($($arg:tt)*) => {};
+    }
+
+    /// Logs a message at the error level.
+    #[macro_export]
+    macro_rules! error {
+        ($($arg:tt)*) => {};
+    }
+
+    /// Prints to the logging output, with a newline.
+    #[macro_export]
+    macro_rules! println {
+        ($($arg:tt)*) => {};
+    }
+}
+
+/// A newtype around byte slices used for all of Ariel OS's logging facades that turns the bytes
+/// into some hex output.
+///
+/// Its preferred output is `00 11 22 33`, but log facades may also produce something like `[00,
+/// 11, 22, 33]` (eg. while that is cheaper on defmt).
+///
+/// Instead of writing some variation of `info!("Found bytes {:02x}", data)`, you can write
+/// `info!("Found bytes {}", Hex(data))`.
+pub struct Hex<T: AsRef<[u8]>>(pub T);
+
+impl<T: AsRef<[u8]>> core::fmt::Display for Hex<T> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "{:02x?}", self.0.as_ref())
+    }
+}
+
+#[cfg(feature = "defmt")]
+impl<T: AsRef<[u8]>> defmt::Format for Hex<T> {
+    fn format(&self, f: defmt::Formatter<'_>) {
+        defmt::write!(f, "{=[u8]:02x}", self.0.as_ref());
+    }
+}
+
+/// A newtype around byte slices used for all of Ariel OS's logging facades that prefers
+/// interpreting the data as CBOR.
+///
+/// Its preferred output is CBOR Diagnostic Notation (EDN), but showing hex is also acceptable.
+///
+/// Instead of writing some variation of `info!("Found bytes {:cbor}", item)`, you can write
+/// `info!("Found bytes {}", Cbor(item))`.
+///
+/// Note that using this wrapper is not necessary when using a
+/// [`cboritem::CborItem`](https://docs.rs/cboritem/latest/cboritem/struct.CborItem.html) as it
+/// already does something similar on its own.
+pub struct Cbor<T: AsRef<[u8]>>(pub T);
+
+impl<T: AsRef<[u8]>> core::fmt::Display for Cbor<T> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        Hex(self.0.as_ref()).fmt(f)
+    }
+}
+
+#[cfg(feature = "defmt")]
+impl<T: AsRef<[u8]>> defmt::Format for Cbor<T> {
+    fn format(&self, f: defmt::Formatter<'_>) {
+        defmt::write!(f, "{=[u8]:cbor}", self.0.as_ref());
+    }
+}
